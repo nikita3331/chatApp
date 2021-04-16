17c23fcfca1a03a6280fef62bb0e6ca6a59832fe
Mykyta Brazhynskyy <nikita3331@gmail.com>
2021-04-16 10:06:08 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,48 +15,54 @@ mongoose.connect(db_uri, { useNewUrlParser: true,useUnifiedTopology: true })
 mongoose.connection.on('error', (error) => console.error(error))
 mongoose.connection.once('open', () => console.log('Connected to Database'))
 
-// app.use(express.json())
-// app.use(cors())
-// app.disable('etag');
-// app.use(bodyParser.json({
-//     limit: '50mb',
-//     extended: true
-//   }));
-  
-// app.use(bodyParser.urlencoded({
-//   limit: '50mb',
-//   parameterLimit: 500000,
-//   extended: true 
-// }));
-
-
-// server.use('/users', userRouter)
-// server.use('/todos', todoRouter)
-// server.listen(process.env.PORT||3000, () => console.log('Server Started'))
-// app.get('/*', function(req, res) {
-//   res.sendFile(__dirname+'/public/hello.html');
-// });
-
-
-
-const PORT = process.env.PORT || 3000;
-const INDEX = '/public/hello.html';
-
 const server = express()
+  .use(cors())
+  .use(bodyParser.json({
+    limit: '50mb',
+    extended: true
+  }))
+  .use(bodyParser.urlencoded({
+    limit: '50mb',
+    parameterLimit: 500000,
+    extended: true 
+  }))
   .use('/users', userRouter)
   .use('/todos', todoRouter)
-  .get('/*',(req,res)=>{res.sendFile(INDEX, { root: __dirname })})
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .get('/*',(req,res)=>{res.sendFile('/public/hello.html', { root: __dirname })})
+  .listen(process.env.PORT || 3000, () => console.log(`Listening `));
 
 const wss = new Server({ server });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
+// wss.on('connection', (ws) => {
+//   console.log('Client connected');
+//   ws.on('close', () => console.log('Client disconnected'));
+// });
 
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 5000);
+const clients = {};
+
+wss.on('request', function(request) {
+  var userID = crypto.randomBytes(40).toString('hex')
+  console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
+  // You can rewrite this part of the code to accept only the requests from allowed origin
+  const connection = request.accept(null, request.origin);
+  clients[userID] = connection;
+  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      const dataFromClient = JSON.parse(message.utf8Data);
+      console.log(dataFromClient)
+      connection.sendUTF(JSON.stringify({halko:1}))
+    }
   });
-}, 5000);
+  // user disconnected
+  connection.on('close', function(connection) {
+    console.log((new Date()) + " Peer " + userID + " disconnected.");
+    delete clients[userID];
+    connection.sendUTF(JSON.stringify({halko:1}))
+  });
+});
