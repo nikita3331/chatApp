4c7e90e3cebc8536449b9b7410a2f674de75f7de
Mykyta Brazhynskyy <nikita3331@gmail.com>
2021-04-16 09:44:19 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const server = express()
+// const server = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const userRouter = require('./routes/users')
